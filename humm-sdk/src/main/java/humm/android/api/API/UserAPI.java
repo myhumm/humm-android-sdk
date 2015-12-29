eaad453281f72f1990d1a0a2fdda3691ff036f6e
Jose Alonso García <josealonsogarcia@gmail.com>
2015-12-29 11:39:53 +0100

    Put genres

--- humm-sdk/src/main/java/humm/android/api/API/UserAPI.java
+++ humm-sdk/src/main/java/humm/android/api/API/UserAPI.java
@@ -6,12 +6,14 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -1581,6 +1583,106 @@ public class UserAPI extends HummAPI {
             }
 
             Reader reader = HttpURLConnectionHelper.getHttpConnection(endpoint + "/users/" + idUser + "/playlists", parameters, token);
+            result = new Gson().fromJson(reader, listType);
+
+        } catch (IOException ex) {
+            // HttpUrlConnection will throw an IOException if any 4XX
+            // response is sent. If we request the status again, this
+            // time the internal status will be properly set, and we'll be
+            // able to retrieve it.
+            Log.e("Debug", "error " + ex.getMessage(), ex);
+            //android bug with 401
+            result.setStatus_response(HttpURLConnectionHelper.KO);
+            result.setError_response("Unauthorized");
+
+        } catch (JSONException e) {
+            Log.e("Debug", "error " + e.getMessage(), e);
+            result.setStatus_response(HttpURLConnectionHelper.KO);
+            result.setError_response("error in params");
+        } catch (Exception e) {
+            Log.e("ERROR", "error " + e.getMessage(), e);
+            result.setStatus_response(HttpURLConnectionHelper.KO);
+            result.setError_response("sync error");
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Put genres to current user
+     *
+     * @param likes    Array of Strings with genres that user likes
+     * @param dislikes Array of Strings with genres that user likes
+     * @param listener called when action is completed or when happens a error. The parameter of onComplete method is
+     *                 a list of <code>PlaylistOwnerInt</code>.
+     *                 Is guaranteed that is called in main thread, so is safe call UI elements inside it.
+     */
+    public void putGenres(final ArrayList<String> likes, final ArrayList<String> dislikes, final OnActionFinishedListener listener) {
+        new HummTask<HummMultipleResult<PlaylistOwnerInt>>(new HummTask.Job() {
+            @Override
+            public Object onStart() throws Exception {
+                return putGenres(likes, dislikes);
+            }
+
+            @Override
+            public void onComplete(Object object) {
+                HummMultipleResult<PlaylistOwnerInt> result = (HummMultipleResult<PlaylistOwnerInt>) object;
+
+                if (result == null) {
+                    listener.actionFinished(null);
+                    return;
+                }
+
+                if (HttpURLConnectionHelper.OK.equalsIgnoreCase(result.getStatus_response())) {
+                    listener.actionFinished(result.getData_response());
+                } else {
+                    listener.onError(new HummException(result.getError_response()));
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                listener.onError(new HummException(e.getLocalizedMessage()));
+            }
+        }).start();
+    }
+
+    /**
+     * Get a list of recommended playlists / albums; returns a list of playlist / album objects
+     *
+     * @param likes    Array of Strings with genres that user likes
+     * @param dislikes Array of Strings with genres that user likes
+     */
+    public HummMultipleResult<PlaylistOwnerInt> putGenres(ArrayList<String> likes, ArrayList<String> dislikes) {
+
+        HummMultipleResult<PlaylistOwnerInt> result = new HummMultipleResult<>();
+        try {
+
+            Type listType = new TypeToken<HummMultipleResult<PlaylistOwnerInt>>() {
+            }.getType();
+
+            if (likes == null) {
+                result.setStatus_response(HttpURLConnectionHelper.KO);
+                result.setError_response("likes parameter is mandatory");
+
+                return result;
+            }
+            if (dislikes == null) {
+                result.setStatus_response(HttpURLConnectionHelper.KO);
+                result.setError_response("dislikes parameter is mandatory");
+
+                return result;
+            }
+
+            JSONObject preferences = new JSONObject();
+            preferences.put("like", new JSONArray(likes));
+            preferences.put("dislike", new JSONArray(dislikes));
+
+            JSONObject parameters = new JSONObject();
+            parameters.put("preferences", preferences);
+
+            Reader reader = HttpURLConnectionHelper.putHttpConnection(endpoint + "/users/me/settings", parameters, false, token);
             result = new Gson().fromJson(reader, listType);
 
         } catch (IOException ex) {
