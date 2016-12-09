777037e9c5c1b3e49b5e1ab9c9ab7e1e0632fef7
Jose Alonso García <josealonsogarcia@gmail.com>
2016-12-09 09:17:10 +0100

    getTracksFromChannel and leaveChannel

--- humm-sdk/src/main/java/humm/android/api/API/ChannelsAPI.java
+++ humm-sdk/src/main/java/humm/android/api/API/ChannelsAPI.java
@@ -20,6 +20,8 @@ import humm.android.api.HummTask;
 import humm.android.api.Model.Channel;
 import humm.android.api.Model.ChatMultipleResult;
 import humm.android.api.Model.ChatResult;
+import humm.android.api.Model.Message;
+import humm.android.api.Model.MessageMultipleResult;
 import humm.android.api.OnActionFinishedListener;
 
 /**
@@ -360,6 +362,83 @@ public class ChannelsAPI extends HummAPI {
         return result;
     }
 
+    public void leaveChannel(final String channelId, final OnActionFinishedListener listener) {
+        new HummTask<ChatResult<Channel>>(new HummTask.Job() {
+            @Override
+            public Object onStart() throws Exception {
+                return leaveChannel(channelId);
+            }
+
+            @Override
+            public void onComplete(Object object) {
+                ChatResult<Channel> result = (ChatResult<Channel>) object;
+
+
+                if (result == null) {
+                    listener.actionFinished(null);
+                    return;
+                }
+
+                if (HttpURLConnectionHelper.OK.equalsIgnoreCase(result.getStatus())) {
+                    listener.actionFinished(result.getChannel());
+                } else {
+                    listener.onError(new HummException("Error"));
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                listener.onError(new HummException(e.getLocalizedMessage()));
+            }
+        }).start();
+    }
+
+    public ChatResult<Channel> leaveChannel(String channelId) {
+
+        ChatResult<Channel> result = new ChatResult<Channel>();
+        try {
+
+            Type listType = new TypeToken<ChatResult<Channel>>() {
+            }.getType();
+
+
+//            HummAPI.getInstance().updateUserToken();
+
+            Reader reader = HttpURLConnectionHelper.getHttpConnection(endpoint + "/channels/" + channelId + "/leave", null, token, true);
+
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").create();
+
+            result = gson.fromJson(reader, listType);
+
+        } catch (IOException ex) {
+            // HttpUrlConnection will throw an IOException if any 4XX
+            // response is sent. If we request the status again, this
+            // time the internal status will be properly set, and we'll be
+            // able to retrieve it.
+            Log.e("Debug", "error " + ex.getMessage(), ex);
+            //android bug with 401
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("Unauthorized");
+
+        } catch (JSONException e) {
+            Log.e("Debug", "error " + e.getMessage(), e);
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("error in params");
+        } catch (Exception e) {
+            Log.e("ERROR", "error " + e.getMessage(), e);
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("sync error");
+
+        }
+
+
+        return result;
+    }
+
     public void createChannel(final String nameChannel, final String descriptionChannel, final boolean isPrivateChannel, final OnActionFinishedListener listener) {
         new HummTask<ChatResult<Channel>>(new HummTask.Job() {
             @Override
@@ -438,5 +517,84 @@ public class ChannelsAPI extends HummAPI {
 
         return result;
     }
+
+
+    public void tracksFromChannel(final String channelId, final OnActionFinishedListener listener) {
+        new HummTask<MessageMultipleResult<Message>>(new HummTask.Job() {
+            @Override
+            public Object onStart() throws Exception {
+                return tracksFromChannel(channelId);
+            }
+
+            @Override
+            public void onComplete(Object object) {
+                MessageMultipleResult<Message> result = (MessageMultipleResult<Message>) object;
+
+
+                if (result == null) {
+                    listener.actionFinished(null);
+                    return;
+                }
+
+                if (HttpURLConnectionHelper.OK.equalsIgnoreCase(result.getStatus())) {
+                    listener.actionFinished(result.getMessages());
+                } else {
+                    listener.onError(new HummException("Error"));
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                listener.onError(new HummException(e.getLocalizedMessage()));
+            }
+        }).start();
+    }
+
+    public MessageMultipleResult<Channel> tracksFromChannel(String channelId) {
+
+        MessageMultipleResult<Channel> result = new MessageMultipleResult<Channel>();
+        try {
+
+            Type listType = new TypeToken<MessageMultipleResult<Channel>>() {
+            }.getType();
+
+
+//            HummAPI.getInstance().updateUserToken();
+
+            Reader reader = HttpURLConnectionHelper.getHttpConnection(endpoint + "/channels/" + channelId + "/tracks", null, token, true);
+
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").create();
+
+            result = gson.fromJson(reader, listType);
+
+        } catch (IOException ex) {
+            // HttpUrlConnection will throw an IOException if any 4XX
+            // response is sent. If we request the status again, this
+            // time the internal status will be properly set, and we'll be
+            // able to retrieve it.
+            Log.e("Debug", "error " + ex.getMessage(), ex);
+            //android bug with 401
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("Unauthorized");
+
+        } catch (JSONException e) {
+            Log.e("Debug", "error " + e.getMessage(), e);
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("error in params");
+        } catch (Exception e) {
+            Log.e("ERROR", "error " + e.getMessage(), e);
+
+//            result.setStatus_response(HttpURLConnectionHelper.KO);
+//            result.setError_response("sync error");
+
+        }
+
+
+        return result;
+    }
+
 
 }
